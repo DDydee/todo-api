e39c7844f807a5a3f619d1e94058d52ba4c2bf1d
Ddydee <daniladonut@gmail.com>
2026-01-18 17:32:04 +0300

    little changes

---
+++ prisma.config.ts
@@ -0,0 +1,10 @@
+import '@dotenvx/dotenvx/config';
+import type { PrismaConfig } from 'prisma';
+
+export default {
+  schema: 'prisma/schema.prisma',
+  migrations: {
+    path: 'prisma/migrations',
+  },
+  datasource: { url: process.env.DATABASE_URL! },
+} satisfies PrismaConfig;
